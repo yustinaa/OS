d6e35ae4ccfcb56378e1808bfee59cc32da72e25
Yustina <yustina.martsynkevich@gmail.com>
2025-09-18 08:21:12 +0300

    forgot to add the right main

--- lab1_os/main.cpp
+++ lab1_os/main.cpp
@@ -20,31 +20,25 @@ int main() {
     cout << "Введите количество записей: ";
     cin >> n;
 
-    // Формируем командную строку как в примере
     string cmdCreator = "Creator.exe " + binF + " " + to_string(n);
     char cmdLine[256];
     strcpy_s(cmdLine, cmdCreator.c_str());
 
-    // Как в примере: заполняем STARTUPINFO
     STARTUPINFOA si;
     PROCESS_INFORMATION pi;
     ZeroMemory(&si, sizeof(STARTUPINFOA));
     si.cb = sizeof(STARTUPINFOA);
 
-    // Запускаем процесс как в примере
     if (!CreateProcessA(NULL, cmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
         cout << "Ошибка запуска Creator\n";
         cout << "Press any key to finish.\n";
         _getch();
         return 1;
     }
-
-    // Ждем завершения как в примере
     WaitForSingleObject(pi.hProcess, INFINITE);
     CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
 
-    // Выводим содержимое файла
     ifstream fin(binF, ios::binary);
     employee emp;
     cout << "\nСодержимое бинарного файла:\n";
@@ -58,7 +52,6 @@ int main() {
     cout << "Введите оплату за час: ";
     cin >> pay;
 
-    // Запускаем Reporter аналогично
     string cmdReporter = "Reporter.exe " + binF + " " + reportF + " " + to_string(pay);
     char cmdR[256];
     strcpy_s(cmdR, cmdReporter.c_str());
